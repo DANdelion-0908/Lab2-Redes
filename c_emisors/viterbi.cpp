05147995799df60c452c380dee4f07f7fd9044b4
Eunice Mata <eunice.mata2992@gmail.com>
2025-08-05 00:01:06 -0600

    base for viterbi algorithm

---
+++ c_emisors/viterbi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stdexcept>
+
+using namespace std;
+
+// codificando usando cosidog conolucional
+vector<string> convolutional_enconde(const string &input){
+    vector<string> result;
+    // code that
+    return result;
+}
+
+int main(int arg, char *argv[]){
+    return 0;
+}
